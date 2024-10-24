68bbf041028d8b1c214019da77548a73b5c3eea4
thanglt213 <161811573+thanglt213@users.noreply.github.com>
2024-10-24 09:47:16 +0700

    Update about.py

--- about.py
+++ about.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 def app():
     st.title("About Page")
-    st.write("This is the About Page.")
+    st.write("Đây là một ứng dụng máy học nhằm phát hiện các giao dịch bất thường trong hoạt động kinh doanh bảo hiểm")
