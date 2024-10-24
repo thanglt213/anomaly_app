735578b140314e7ae3293db7c5bace4e3a915952
thanglt213 <161811573+thanglt213@users.noreply.github.com>
2024-10-24 08:09:22 +0700

    Update and rename streamlit_app.py to main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import home
+import about
+import contact
+
+# Tạo menu điều hướng
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Main Menu",
+        options=["Home", "About", "Contact"],
+        icons=["house", "info-circle", "envelope"],
+        menu_icon="cast",
+        default_index=0,
+    )
+
+# Gọi app tương ứng theo lựa chọn của người dùng
+if selected == "Home":
+    home.app()
+elif selected == "About":
+    about.app()
+elif selected == "Contact":
+    contact.app()
+
