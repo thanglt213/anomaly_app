7911af4b94f72950cce4b0997da5eec9cd2b31c1
thanglt213 <161811573+thanglt213@users.noreply.github.com>
2024-10-28 17:14:07 +0700

    Update home.py

--- home.py
+++ home.py
@@ -184,10 +184,8 @@ def ke_toan_option():
         if kt_new_file is not None:
             kt_new_data = pd.read_csv(kt_new_file)
             st.session_state['kt_new_data'] = kt_new_data
-            # st.dataframe(kt_new_data.head())
             predicted_data = predict_with_kmeans_model(kt_kmeans, kt_scaler, kt_new_data, KMEANS_NUMERIC_FEATURES)
             st.session_state['kt_predicted_data'] = predicted_data
-            # st.dataframe(predicted_data.head())
 
     # Hiển thị dữ liệu dự đoán và nút tải xuống nếu có dữ liệu dự đoán
     if st.session_state['kt_predicted_data'] is not None:
@@ -197,12 +195,7 @@ def ke_toan_option():
                            data=st.session_state['kt_predicted_data'].to_csv(index=False).encode('utf-8'), 
                            file_name='kmeans_prediction_results.csv', 
                            mime='text/csv')
-    '''
-    # Hiển thị dữ liệu huấn luyện nếu đã được tải lên
-    if st.session_state['kt_train_data'] is not None:
-        st.write("Dữ liệu huấn luyện đã tải lên:")
-        st.dataframe(st.session_state['kt_train_data'].head())
-    '''
+
 # Modul bảo hiểm sức khỏe        
 def suc_khoe_option():
     # Khởi tạo các dữ liệu cần thiết nếu chưa có trong session state
